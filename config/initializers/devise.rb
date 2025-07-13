430191fa0072893317376393dab70c183b2de102
AlvesLuis0 <alves.luis.3002@gmail.com>
2025-07-13 15:38:11 -0300

    refactor: remove unnecessary configurations

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,13 +313,6 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
-    jwt.dispatch_requests = [
-      ['POST', %r{^/login$}],
-      ['POST', %r{^/signup$}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
-    ]
     jwt.expiration_time = 8.hours
   end
 end
